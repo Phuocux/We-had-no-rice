e16a1a80208287d12733b99d9e05df8ac459d507
Phuocux <phuocuv@gmail.com>
2026-06-03 19:11:41 +0700

    sửa lko hiển thị update tiền

--- Assets/Scripts/Misc/EconomyManager.cs
+++ Assets/Scripts/Misc/EconomyManager.cs
@@ -5,11 +5,17 @@ public class EconomyManager : Singleton<EconomyManager>
 {
     [SerializeField] private TMP_Text goldText;
     private int currentGold = 0;
+    const string COIN_AMOUNT_TEXT = "Gold Amount Text";
 
     public void UpdateCurrentGold()
     {
         currentGold += 1;
-        if (goldText != null)
-            goldText.text = currentGold.ToString("D3");
+        if (goldText == null)
+        {
+            goldText = GameObject.Find(COIN_AMOUNT_TEXT).GetComponent<TMP_Text>();
+        }
+
+        goldText.text = currentGold.ToString("D3");
     }
+
 }
