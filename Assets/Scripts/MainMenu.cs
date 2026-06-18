4f6ec0eb198c6085bdb400ca44c29438a2634125
Dau Vuong <dauvuong@gmail.com>
2026-06-18 10:01:31 +0700

    mainmenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject optionsMenu;
+    public GameObject mainMenu;
+
+    public void OpenOptionsPanel()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void OpenMainMenuPanel()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("Bscene 1");
+    }
+}
